dc1b05c14c1a1a0733ab580719b51ca2a57c0862
Daniele Mammarella <dmammare@redhat.com>
2024-02-18 23:21:48 +0100

    first commit

---
+++ src/main/java/org/example/testInfinispanCoreDependencyWithJgroup/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package org.example.testInfinispanCoreDependencyWithJgroup;
+
+import org.jgroups.nio.Buffers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.example.testInfinispanCoreDependencyWithJgroupJar.MyClass.extractVersion;
+
+@WebServlet("/HelloWorld")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final PrintWriter writer = resp.getWriter();
+        writer.println("Buffers package: " + extractVersion(Buffers.class));
+        writer.close();
+    }
+}
