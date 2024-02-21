b19ad0f4724860dc85104c20f914a3b83c1d79d8
Daniele Mammarella <dmammare@redhat.com>
2024-02-21 15:35:11 +0100

    - wildfly:deploy works fine with jdk8

--- src/main/java/org/example/testInfinispanCoreDependencyWithJgroup/HelloWorldServlet.java
+++ src/main/java/org/example/testInfinispanCoreDependencyWithJgroup/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package org.example.testInfinispanCoreDependencyWithJgroup;
 
 import org.infinispan.Cache;
-import org.jgroups.nio.Buffers;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +18,6 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final PrintWriter writer = resp.getWriter();
         writer.println("Cache package: " + extractVersion(Cache.class));
-        //writer.println("Buffers package: " + extractVersion(Buffers.class));
         writer.close();
     }
 }
